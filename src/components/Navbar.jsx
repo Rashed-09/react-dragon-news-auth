68688791cd78efca2ede92a4327b269332935b88
Rashed-09 <rashedmdabdur452@gmail.com>
2025-01-01 21:57:59 +0600

    added login route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,15 @@ const Navbar = () => {
         <Link to="/">Home</Link>
         <Link to="/career">Career</Link>
         <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
       </div>
       <div className="login flex gap-2 items-center">
         <div className=" ">
           <img src={userIcon} alt="" />
         </div>
-        <button className="btn btn-neutral rounded-none">Login</button>
+        <Link to={"/login"}>
+          <button className="btn btn-neutral rounded-none">Login</button>
+        </Link>
       </div>
     </div>
   );
